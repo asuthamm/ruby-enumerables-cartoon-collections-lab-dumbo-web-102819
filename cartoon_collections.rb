38696fca792d054cfa6a5f982f8d02f7d0279888
Alex Suthammanont <asuthamm@gmail.com>
2019-10-23 18:39:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(arr) # code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr) # code an argument here
+  new_arr = []
+  arr.map { |word| word.capitalize + "!"
+     
+  end
 end
 
 def long_planeteer_calls# code an argument here
