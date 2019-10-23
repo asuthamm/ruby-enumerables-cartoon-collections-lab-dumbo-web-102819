65acc8bafc75ea43593bf712c384e4a76359d076
Alex Suthammanont <asuthamm@gmail.com>
2019-10-23 19:03:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,12 @@ def long_planeteer_calls(arr) # code an argument here
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  arr.each do |word|
+    return word if cheese_types.include?(word)
+  end
+  return nil
 end
